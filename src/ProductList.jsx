87ab7c30f8850fa2ac7eb7c3eb0ec8b1daf57c11
sam-349 <samuelraj349@gmail.com>
2024-08-06 14:19:34 -0400

    update productlist

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,6 @@
 import React, { useState,useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './ProductList.css'
-import {addItem} from './CreatSlice'
 import Cart from './CartItem';
 
 function ProductList() {
